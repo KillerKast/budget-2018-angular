07d2c7315ed93fdaf4e2711fccf9e9b8a28e8138
KillerKast <Jonathan.Kast312@gmail.com>
2018-05-20 11:21:22 -0500

    Monthly Bills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { MonthlyBillEntryComponent } from './monthly-bill-entry/monthly-bill-entry.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {
+  MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule,
+  MatNativeDateModule
+} from "@angular/material";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonthlyBillEntryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/monthly-bill-entry/MonthlyBill.ts
@@ -0,0 +1,6 @@
+export class MonthlyBill {
+  id: string;
+  name: string;
+  amount: number;
+  paymentDate: any;
+}
